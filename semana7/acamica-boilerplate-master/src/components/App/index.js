fa1a0ba42b97ef2cede2e10a2cb470267475e3ee
franciscoescobar <franciscoescobar@users.noreply.github.com>
2019-05-02 22:10:35 -0300

    Refactored with useEffect App compontent

--- semana7/acamica-boilerplate-master/src/components/App/index.js
+++ semana7/acamica-boilerplate-master/src/components/App/index.js
@@ -16,15 +16,17 @@ const App = () => {
             rooms: undefined
             },
             hotels: [],
-            loading: true
+            loading: true,
+            filteredHotels: []
         }
     )
-    const {filters, hotels, loading} = state    
+    const {filters, hotels, loading, filteredHotels} = state    
     const fetchUrl = async () => {
         try {
             const response = await fetch('https://wt-8a099f3e7c73b2d17f4e018b6cfd6131-0.sandbox.auth0-extend.com/acamica')
             const json = await response.json()
-            setState({hotels: json, loading: false})
+            console.log(json)
+            setState({hotels: json, loading: false, filteredHotels: json})
 
         } catch (error) {
             console.log(error)
@@ -42,11 +44,14 @@ const App = () => {
             filters: payload
         })
     }
-    const filterHotelsByCountry = hotels.filter(hotel => hotel.country === filters.country || filters.country === undefined || filters.country === 'Todos los países')
-    const filterHotelsByPrice = filterHotelsByCountry.filter(hotel => hotel.price == filters.price || filters.price === undefined || filters.price === 'Cualquier precio')
-    const filterHotelsByRooms = filterHotelsByPrice.filter(hotel => (filters.rooms - 10 <= hotel.rooms && hotel.rooms <= filters.rooms) || filters.rooms === undefined || filters.rooms === 'Cualquier tamaño')
-    const filteredHotels = filterHotelsByRooms.filter(hotel => hotel.availabilityFrom <= filters.dateFrom && hotel.availabilityTo >= filters.dateTo)
-
+    useEffect(() => {
+        const filterHotelsByCountry = hotels.filter(hotel => hotel.country === filters.country || filters.country === undefined || filters.country === 'Todos los países')
+        const filterHotelsByPrice = filterHotelsByCountry.filter(hotel => hotel.price == filters.price || filters.price === undefined || filters.price === 'Cualquier precio')
+        const filterHotelsByRooms = filterHotelsByPrice.filter(hotel => (filters.rooms - 10 <= hotel.rooms && hotel.rooms <= filters.rooms) || filters.rooms === undefined || filters.rooms === 'Cualquier tamaño')
+        const filteredHotels = filterHotelsByRooms.filter(hotel => hotel.availabilityFrom <= filters.dateFrom && hotel.availabilityTo >= filters.dateTo)
+        setState({filteredHotels})
+    }, [filters])
+    
    
     return (
         <div>
